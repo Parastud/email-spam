a52d91335a084481245cf5963aec713df10c424d
Parth <parthsharma07070@gmail.com>
2025-10-28 14:48:21 +0530

    relative path

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
 
-
 def transform_text(text):
     text = text.lower()
     text = nltk.word_tokenize(text)
@@ -33,22 +32,17 @@ def transform_text(text):
 
     return " ".join(y)
 
-tfidf = pickle.load(open(r'D:\testing_spam\vectorizer.pkl','rb'))
-model = pickle.load(open(r'D:\testing_spam\model.pkl','rb'))
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
 
 st.title("Email/SMS Spam Classifier")
 
 input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
-
-    # 1. preprocess
     transformed_sms = transform_text(input_sms)
-    # 2. vectorize
     vector_input = tfidf.transform([transformed_sms])
-    # 3. predict
     result = model.predict(vector_input)[0]
-    # 4. Display
     if result == 1:
         st.header("Spam")
     else:
